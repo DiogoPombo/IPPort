505bd73684f48d1735ec400f29f7a4d0a19cb3c5
DiogoPombo <150866276+DiogoPombo@users.noreply.github.com>
2024-11-28 14:03:22 -0300

    Add files via upload

---
+++ IPPort.bat
@@ -0,0 +1,65 @@
+@echo off
+@title IPPort
+set APP=IPPort
+set AUTHOR=POMBO
+set AVATAR=\Õ/
+set MADE_BY=MADE BY:
+set SPACE= 
+set KEY=@EWEP - 2024
+set HEADER=************************* IPPort *************************
+set DESCRIPTION=This script tests an IP/URL and the port (optional).
+
+:start
+setlocal
+COLOR F0
+echo %APP%%SPACE%%MADE_BY%%SPACE%%SPACE%%AUTHOR%%SPACE%%SPACE%%AVATAR%%SPACE%%KEY%
+echo.
+echo %SPACE%%SPACE%%SPACE%%HEADER%
+echo.
+echo %SPACE%%DESCRIPTION%
+echo.
+timeout /t 1 /nobreak >nul
+
+echo %SPACE%Waiting for data. . .
+set "prompt=Enter an IP or URL:"
+for /f "tokens=*" %%i in ('powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%prompt%', 'IPPort')"') do set "ip=%%i"
+
+if "%ip%"=="" (
+    cls
+    COLOR F4
+    echo %SPACE%%SPACE%%SPACE%%HEADER%
+    echo.
+    echo %SPACE%ERROR: No IP or URL entered!
+    goto end
+)
+
+set "prompt=Enter the port (optional):"
+for /f "tokens=*" %%i in ('powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%prompt%', 'IPPort')"') do set "port=%%i"
+
+cls
+
+if "%port%"=="" (
+    echo %SPACE%%SPACE%%SPACE%%HEADER%
+    echo.
+    echo %SPACE%Testing %ip%. . .
+    echo.
+    powershell -Command "Test-NetConnection -ComputerName %ip%"
+) else (
+    echo %SPACE%%SPACE%%SPACE%%HEADER%
+    echo.
+    echo Testing %ip% on port %port%. . .
+    echo.
+    powershell -Command "Test-NetConnection -ComputerName %ip% -Port %port%"
+)
+
+echo Pinging %ip%. . .
+powershell -Command "ping %ip%"
+echo.
+
+:end
+endlocal
+powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Do you want to run another test?', 'IPPort', [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)" | find "Yes" >nul
+if %errorlevel%==0 (
+    cls
+    goto start
+)
