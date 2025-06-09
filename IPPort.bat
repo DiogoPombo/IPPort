f9f072bf4b340801139126315e40422870b6af2e
DiogoPombo <150866276+DiogoPombo@users.noreply.github.com>
2025-06-09 18:32:21 -0300

    Add files via upload

--- IPPort.bat
+++ IPPort.bat
@@ -1,17 +1,20 @@
 @echo off
 @title IPPort
-set APP=IPPort
-set AUTHOR=POMBO
-set AVATAR=\Õ/
-set MADE_BY=MADE BY:
-set SPACE= 
-set KEY=@EWEP - 2025
-set HEADER=************************* IPPort *************************
-set DESCRIPTION=This script tests an IP/URL and the port (optional).
 
 :start
 setlocal
 COLOR F0
+set APP=IPPort
+set AUTHOR=POMBO
+set AVATAR=\\Õ/
+set MADE_BY=MADE BY:
+set SPACE= 
+set KEY=@EWEP - 2025
+set HEADER=******************** IPPort ********************
+set DESCRIPTION=This script tests an IP/URL and an optional port.
+set /a randNum=%random% %% 9000 + 1000
+set "userAgent=IPPort%randNum%"
+
 echo %APP%%SPACE%%MADE_BY%%SPACE%%SPACE%%AUTHOR%%SPACE%%SPACE%%AVATAR%%SPACE%%KEY%
 echo.
 echo %SPACE%%SPACE%%SPACE%%HEADER%
@@ -20,11 +23,11 @@ echo %SPACE%%DESCRIPTION%
 echo.
 timeout /t 1 /nobreak >nul
 
-echo %SPACE%Waiting for data. . .
+echo %SPACE%Waiting for input...
 set "prompt=Enter an IP or URL:"
-for /f "tokens=*" %%i in ('powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%prompt%', 'IPPort')"') do set "ip=%%i"
+for /f "tokens=*" %%i in ('powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%prompt%', 'IPPort')"') do set "entrada=%%i"
 
-if "%ip%"=="" (
+if "%entrada%"=="" (
     cls
     COLOR F4
     echo %SPACE%%SPACE%%SPACE%%HEADER%
@@ -33,38 +36,87 @@ if "%ip%"=="" (
     goto end
 )
 
-set "prompt=Enter the port (optional):"
-for /f "tokens=*" %%i in ('powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%prompt%', 'IPPort')"') do set "port=%%i"
+set "host="
+echo %entrada% | findstr "http://" >nul && set "host=%entrada:~7%"
+echo %entrada% | findstr "https://" >nul && set "host=%entrada:~8%"
+if not defined host set "host=%entrada%"
+
+for /f "tokens=1 delims=/" %%i in ("%host%") do set "host=%%i"
+
+echo.
+echo %SPACE%Detected host: %host%
+timeout /t 1 /nobreak >nul
+echo.
+
+echo %host% | findstr /i ".onion .xyz .ru" >nul
+if %errorlevel%==0 (
+    cls
+    COLOR F4
+    echo %SPACE%%SPACE%%SPACE%%HEADER%
+    echo.
+    echo %SPACE%ERROR: Potentially malicious host detected!
+    goto end
+)
+
+set "prompt=Enter port (optional):"
+for /f "tokens=*" %%i in ('powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%prompt%', 'IPPort')"') do set "porta=%%i"
 
 cls
 
-if "%port%"=="" (
+if "%porta%"=="" (
     echo %SPACE%%SPACE%%SPACE%%HEADER%
     echo.
-    echo %SPACE%Testing %ip%. . .
+    echo %SPACE%Testing %host%...
+    timeout /t 1 /nobreak >nul
     echo.
-    powershell -Command "Test-NetConnection -ComputerName %ip% -InformationLevel Detailed"
+    powershell -Command "Test-NetConnection -ComputerName %host% -InformationLevel Detailed"
 ) else (
     echo %SPACE%%SPACE%%SPACE%%HEADER%
     echo.
-    echo %SPACE%Testing %ip% on port %port%. . .
+    echo %SPACE%Testing %host% on port %porta%...
+    timeout /t 1 /nobreak >nul
     echo.
-    powershell -Command "Test-NetConnection -ComputerName %ip% -Port %port% -InformationLevel Detailed"
+    powershell -Command "Test-NetConnection -ComputerName %host% -Port %porta% -InformationLevel Detailed"
 )
 
-echo %SPACE%Performing simple ping on %ip%. . .
-powershell -Command "ping %ip%"
+echo %SPACE%Executing simple ping on %host%...
+timeout /t 1 /nobreak >nul
+powershell -Command "ping %host%"
 echo.
 echo.
 echo.
 
-echo %SPACE%Tracking %ip%. . .
-powershell -Command "tracert %ip%"
+echo %SPACE%Looking up DNS...
+timeout /t 1 /nobreak >nul
+echo.
+powershell -Command "nslookup %host%"
+echo.
+echo.
+echo.
+
+echo %SPACE%Executing simple curl on %host%...
+timeout /t 1 /nobreak >nul
+echo.
+echo %SPACE%Generated User-Agent: %userAgent%
+timeout /t 1 /nobreak >nul
+echo.
+curl.exe -I --max-time 10 --user-agent "%userAgent%" %host%
+echo.
+echo.
+echo.
+
+echo %SPACE%Tracing route to %host%...
+timeout /t 1 /nobreak >nul
+powershell -Command "tracert %host%"
 echo.
 
 :end
 endlocal
-powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Do you want to run another test?', 'IPPort', [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)" | find "Yes" >nul
+timeout /t 1 /nobreak >nul
+
+
+powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Do you want to perform another test?', 'IPPort', [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)" | find "Yes" >nul
+
 if %errorlevel%==0 (
     cls
     goto start
